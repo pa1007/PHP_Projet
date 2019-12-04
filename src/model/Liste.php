9e8281f682475c3961d61a2155fb288929f2bba0
Lasartus <mat54940@hotmail.fr>
2019-12-04 15:35:05 +0100

    création liste

--- src/model/Liste.php
+++ src/model/Liste.php
@@ -10,8 +10,22 @@ class Liste extends Model {
     protected $primaryKey = "no";
     protected $table = "liste";
 
-    public function Item(){
-        return $this->hasMany('mywhishlist\model\Item','user_id');
+
+    public function Item()
+    {
+        return $this->hasMany('mywhishlist\model\Item', 'user_id');
+    }
+
+    public function creerListe($n,$us,$tit,$des,$exp,$tok){
+        $l= new Liste();
+        $l->no = $n;
+        $l->user_id=$us;
+        $l->titre=$tit;
+        $l->description=$des;
+        $l->expiration=$exp;
+        $l->token=$tok;
+
+        $l->save();
     }
 
 }
