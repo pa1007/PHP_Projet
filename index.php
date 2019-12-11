9d5ad585a511b67ac1070d4486fe72939988555b
DROMMER Fabien <Fabiendro@outlook.fr>
2019-12-11 15:01:28 +0100

    ajout routes

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 use Illuminate\Database\Capsule\Manager as DB;
 use mywishlist\controller\ItemController;
 use mywishlist\controller\ListController;
+use mywishlist\controller\ModifController;
 use mywishlist\vue\VueError;
 use Slim\Slim;
 
@@ -23,6 +24,16 @@ $app->get('/error', function () {
     $vueError = new VueError();
     $vueError->render(1);
 })->setName("Error");
+
+$app->get("/modif/:type/:token", function ($type, $token) {
+    $modifController = new ModifController($type, filter_var($token, FILTER_SANITIZE_STRING));
+    $modifController->process();
+});
+$app->post("/modif/:type/:token", function ($type, $token) {
+    $modifController = new ModifController($type, filter_var($token, FILTER_SANITIZE_STRING));
+    $modifController->modifyItem();
+});
+
 
 $db = new DB();
 $db->addConnection(parse_ini_file("src/conf/conf.ini"));
