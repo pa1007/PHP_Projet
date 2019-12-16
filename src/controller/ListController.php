5ace9d6313422811e4e27991bc0b636cb67e4c1f
pa1007 <pa.fourriere@pa1007.fr>
2019-12-16 14:50:36 +0100

    fix

--- src/controller/ListController.php
+++ src/controller/ListController.php
@@ -4,36 +4,20 @@
 namespace mywishlist\controller;
 
 
-use mywishlist\model\Item;
 use mywishlist\model\Liste;
 use mywishlist\vue\VueParticipant;
 use Slim\Slim;
 
-class ListController
-{
+class ListController {
+    public function __construct() {
 
-
-    public function __construct(){
     }
 
-    public function getList( $no )
-    {
+    public function getList($no) {
         $list = Liste::where('no', '=', $no)->first();
         $v = new VueParticipant($list);
         $v->render(VueParticipant::LISTE);
     }
-
-    private function testToken() {
-        if (isset($_SESSION['token'])) {
-            foreach ($_SESSION['token'] as $item) {
-                if ($item === $this->token) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 
     public function seeFormCrea() {
         $msg = "";
@@ -48,50 +32,22 @@ class ListController
     public function postCreaForm() {
         $slim = Slim::getInstance();
         $it = 0;
-        if (isset($_POST['titre']) && $_POST["titre"] !== "" && isset($_POST["Description"]) && $_POST["Description"] !== "" && isset($_POST["datexp"])
-            && $_POST["datexp"] !== "" && isset($_POST['singlebutton']) && $_POST['singlebutton'] === "Submit") {
+        if (isset($_POST['titre']) && $_POST["titre"] !== "" && isset($_POST["Description"]) && $_POST["Description"] !== "" && isset($_POST["date"])
+            && $_POST["date"] !== "" && isset($_POST['valider'])) {
             $list = new Liste();
             $list->titre = filter_var($_POST['titre'], FILTER_SANITIZE_SPECIAL_CHARS);
-            $list->descr = filter_var($_POST['Description'], FILTER_SANITIZE_SPECIAL_CHARS);
-            $list->tarif = filter_var($_POST['datexp'], FILTER_SANITIZE_STRING);
-            if (isset($_POST['urlExter'])) {
-                $list->url = filter_var($_POST['urlExter'], FILTER_SANITIZE_URL);
-            }
-            $list->liste_id = 0;
+            $list->description = filter_var($_POST['Description'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $list->expiration = filter_var($_POST['date'], FILTER_SANITIZE_STRING);
             $list->modifToken = bin2hex(openssl_random_pseudo_bytes(32));
             $list->save();
             $it = $list->id;
             $_SESSION['token'][] = $list->modifToken;
-
-        } else {
-            setcookie("Error", "Il y a une erreur dans le formulaire", time() + 10);
-            $slim->redirect($slim->urlFor("creaListe"), 302);
-        }
-        $req = $slim->request;
-        $url = $req->getRootUri() . "/liste/$it";
-        $slim->redirect($url, 302);
-
-    }
-
-    public function creerListe(){
-        $slim = Slim::getInstance();
-        $req = $slim->request;
-        $liste = Liste::where("modifToken", "=", $this->token)->first();
-        $titre  = $_POST['titre'];
-        $description = $_POST['Description'];
-        $datexp = $_POST['datexp'];
-        $butP = $_POST['submit'];
-        if ($titre!== "" && $description !== "" && $datexp !== "" && $butP === 'submit') {
-            $liste->titre = filter_var($titre, FILTER_SANITIZE_SPECIAL_CHARS);
-            $liste->descr = filter_var($description, FILTER_SANITIZE_SPECIAL_CHARS);
-            $liste->datex = filter_var($datexp, FILTER_SANITIZE_NUMBER_FLOAT);
-            $liste->url = filter_var($_POST['urlEx'], FILTER_SANITIZE_URL);
-            $liste->save();
-            $url = $req->getRootUri() . "/item/$liste->id";
+            $req = $slim->request;
+            $url = $req->getRootUri() . "/modif/liste/$list->modifToken";
             $slim->redirect($url, 302);
         } else {
             setcookie("Error", "Il y a une erreur dans le formulaire", time() + 10);
-            $slim->redirect($req->getResourceUri(), 302);
+            $slim->redirect($slim->urlFor("creaListe"), 302);
         }
 
 
